1566e266c9725e07f9a80e7f65762de9812d7ba4
Alex Cerjanic <acerjanic@users.noreply.github.com>
2019-07-22 14:36:07 -0500

    Merging in pre-deployment updates (#14)
    * Adding systemd service file for the worker
    
    * Add output back to s3 output bucket in tasks.py
    
    * Updating requirements.txt for deployment

--- spinveyor/SV_Worker/tasks.py
+++ spinveyor/SV_Worker/tasks.py
@@ -13,11 +13,14 @@ def submit_job_to_queue(recontype, bucket, senfm, imgdata, subjectID):
     # Start by forming the nextflow command
     protonHome = os.environ['PROTON_HOME']
     nextflowBin = os.environ['NEXTFLOW_BIN']
+    outBucket = os.environ['SPINVEYOR_OUTPUT_BUCKET']
     s3urlSenMap = 's3://' + bucket + '/' + senfm
     s3urlImgData = 's3://' + bucket + '/' + imgdata
+    s3urlOutDir = 's3://' + outBucket + '/' + subjectID
     nfCommand = (nextflowBin + ' run ' + 'recon' + recontype + '.nf ' + ' --senfm ' +
                 s3urlSenMap + ' --imgData ' + s3urlImgData + ' --subjectID ' + 
-                subjectID + ' --protonHome ' + protonHome)
+                subjectID + ' --protonHome ' + protonHome + ' --outDir ' + s3urlOutDir)
+                
     workDir = protonHome + '/spinveyor/recon'
     print(s3urlSenMap)
     print(s3urlImgData)
