c8eec311545cd29d572078e15bc09b6f143bee85
Alex Cerjanic <acerja2@illinois.edu>
2019-07-22 20:38:15 -0500

    Put the recon'ed file in the right S3 bucket

--- spinveyor/SV_Worker/tasks.py
+++ spinveyor/SV_Worker/tasks.py
@@ -16,7 +16,7 @@ def submit_job_to_queue(recontype, bucket, senfm, imgdata, subjectID):
     outBucket = os.environ['SPINVEYOR_OUTPUT_BUCKET']
     s3urlSenMap = 's3://' + bucket + '/' + senfm
     s3urlImgData = 's3://' + bucket + '/' + imgdata
-    s3urlOutDir = 's3://' + outBucket + '/' + subjectID
+    s3urlOutDir = 's3://' + outBucket
     nfCommand = (nextflowBin + ' run ' + 'recon' + recontype + '.nf ' + ' --senfm ' +
                 s3urlSenMap + ' --imgData ' + s3urlImgData + ' --subjectID ' + 
                 subjectID + ' --protonHome ' + protonHome + ' --outDir ' + s3urlOutDir)
