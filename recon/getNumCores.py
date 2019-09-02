90f4e77bed5e24c3b31273954dc802ecb85d870b
Alex Cerjanic <acerja2@illinois.edu>
2019-09-02 13:17:02 -0500

    Updating getNumCores helper program

--- recon/getNumCores.py
+++ recon/getNumCores.py
@@ -1,2 +1,17 @@
 import psutil
-print(psutil.cpu_count(logical = False), end='')
+import click
+
+@click.command()
+@click.option("--maxCores", default=0, help='use to set a maximum number of cores to report.')
+def getNumCores(maxcores):
+    """ Driver program to return max number of physical cores """
+
+    physNumCores = psutil.cpu_count(logical = False)
+    if maxcores != 0:
+        if maxcores < physNumCores:
+            physNumCores = maxcores
+
+    print(physNumCores, end='')
+
+if __name__ == '__main__':
+    getNumCores()
